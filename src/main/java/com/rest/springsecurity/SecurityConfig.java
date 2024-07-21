546078ec667369495fa7d4af9ca0d5a39ace5cac
SesneiBunny <lukasmatrix12345@gmail.com>
2024-07-21 23:02:35 +0200

    In memory user menagament

---
+++ src/main/java/com/rest/springsecurity/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.rest.springsecurity;
+
+import lombok.EqualsAndHashCode;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        //http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User.withUsername("user1").password("{noop}123").roles("USER").build(); // user1
+
+        UserDetails admin = User.withUsername("admin").password("{noop}123").roles("ADMIN").build(); // user1
+
+        return new InMemoryUserDetailsManager(user1,admin);
+    }
+}
